eef42d24d34327a4337faf527c4548d79b992184
acealone <elwinbeck@googlemail.com>
2024-01-08 21:30:54 +0100

    RAM changes

--- main.js
+++ main.js
@@ -37,9 +37,41 @@ this.className = "empty";
     
 }
 function dragDrop(){
-this.className = "empty";
-const hold = document.querySelector(".invisible");
-if(this.children.length < 1){
-this.append(hold);}
+    this.className = "empty";
+    const hold = document.querySelector(".invisible");
+    if(this.children.length < 1) {
+        this.append(hold);
+        if (checkWin()) {
+            alert('YOU WON!');
+        }
+    }
+
+}
+
+function checkWin() {
+    const first = document.querySelector('.first');
+    const second = document.querySelector('.second');
+    const fourth = document.querySelector('.fourth');
+    const fifth = document.querySelector('.fifth');
+    const sixth = document.querySelector('.sixth');
+    const seventh = document.querySelector('.seventh');
+    const eighth = document.querySelector('.eighth');
+    const ninth = document.querySelector('.ninth');
+    const firstContainer = document.querySelector('#first');
+    const secondContainer = document.querySelector('#second');
+    const fourthContainer = document.querySelector('#fourth');
+    const fifthContainer = document.querySelector('#fifth');
+    const sixthContainer = document.querySelector('#sixth');
+    const seventhContainer = document.querySelector('#seventh');
+    const eighthContainer = document.querySelector('#eighth');
+    const ninthContainer = document.querySelector('#ninth');
+    return firstContainer.contains(first)
+        && secondContainer.contains(second)
+        && fourthContainer.contains(fourth)
+        && fifthContainer.contains(fifth)
+        && sixthContainer.contains(sixth)
+        && seventhContainer.contains(seventh)
+        && eighthContainer.contains(eighth)
+        && ninthContainer.contains(ninth);
 
 }
